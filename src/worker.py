8cdd6a5beb18ed29cf78519cbf7f4a6cbedc7665
System Administrator <root@admins-MacBook-Pro-102.local>
2019-08-20 22:12:40 +0530

    update to worker

--- src/worker.py
+++ src/worker.py
@@ -10,11 +10,11 @@ ALERT_REMAIN_TIME_MAX = 65
 EXTEND_REMAIN_TIME = 60
 
 # run a reminder for every 10 mins
-@tl.job(interval=timedelta(seconds=6))
+@tl.job(interval=timedelta(seconds=600))
 def cl_reminder_task():
     print("cl_reminder_task : ", time.ctime())
 
-    time_stamp = time.time()
+    time_stamp = time.time() * 1000
 
     print("do reminder task at : ", time_stamp)
     # go though all CL list of xyz table, and get remaining time for each CL.
@@ -28,13 +28,14 @@ def cl_reminder_task():
             try:
                 print("server: ", server_id , " , and info: ", server_info)
 
-                remain_time = server_info["remaining_time"]
+                end_time = server_info["end_time"]
+                remain_time = (end_time - time_stamp) / 60000 # divide by 60 sec*10000 ms to get minutes
 
-                if remain_time is None:
-                    print("remain_time is None, not a valid case server_info: {}".format(server_info))
+                if remain_time < 1:
+                    print("remain_time is negative, not a valid case server_info: {}".format(server_info))
                 elif ALERT_REMAIN_TIME_MIN < remain_time < ALERT_REMAIN_TIME_MAX:
                     print("remain team {} is in range to raise a alert, and server_info: {} ".format(remain_time, server_info))
-                    raise_alert(server_id, server_info)
+                    raise_alert(server_id, server_info, remain_time)
                 else:
                     print("no alert needed server_info: {}".format(remain_time))
 
@@ -44,16 +45,13 @@ def cl_reminder_task():
     except Exception as e:
         print("cl_reminder_task: Exception: {}".format(e))
 
-
 # extension task for every 10 mins
 @tl.job(interval=timedelta(seconds=6000))
 def cl_keepalive_task():
 
-    try:
-        print("cl_keepalive_task :", time.ctime())
+    print("cl_keepalive_task :", time.ctime())
 
-    except Exception as e:
-        print("cl_keepalive_task: Exception: {}".format(e))
+    time_stamp = time.time() * 1000
 
     # go though all CL list of xyz table, and get remaining time for each CL.
     # If remaining time is less than 30 mins and counter is non zero, extend the life.
@@ -69,8 +67,10 @@ def cl_keepalive_task():
                     print(" nothign to do, keep alive is false or counter is 0 for server_id:", server_id)
                     continue
 
-                remain_time = server_info["remaining_time"]
+                end_time = server_info["end_time"]
                 counter = server_info["counter"]
+                remain_time = (end_time - time_stamp) / 60000 # divide by 60 sec*10000 ms to get minutes
+
                 if remain_time is None:
                     print("remain_time is None, not a valid case server_info: {}".format(server_info))
                 elif remain_time < EXTEND_REMAIN_TIME:
@@ -89,18 +89,29 @@ def cl_keepalive_task():
 def get_servers_list():
 
     servers = {
-        "123": {"users_list": [], "remaining_time": 60, "keep_alive": False, "counter": 0},
-
-        "223": {"users_list": [], "remaining_time": 66, "keep_alive": False, "counter": 0}
+        "123": {"users_list": [], "end_time": None, "keep_alive": False, "counter": 0, "server_name": None,  "server_type": "Central-Lite"}
         }
 
     return servers
 
 
-def raise_alert(server_id, server_info):
+def raise_alert(server_id, server_info, remain_time):
     print("********************************************************************************")
     print("raised alert for server_id: {} , server_info:{}".format(server_id, server_info))
     print("********************************************************************************")
+    users_list = server_info["users_list"]
+
+    vm_list = []
+    vm1 = {}
+    vm1["server_id"] = server_id
+    vm1["server_name"] = server_info("server_name")
+    vm1["server_status"] = "ON"
+    vm1["remain_time"] = remain_time + " Mins"
+    vm1["server_type"] = "Central-Lite"
+    vm_list.append(vm1)
+
+    post_to_slack(users_list, "alert")
+#  post_to_slack(channel_id, msg_type, vm_list):
 
 
 def extend_server(server_id, server_info):
